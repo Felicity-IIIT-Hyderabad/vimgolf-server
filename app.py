ec86f585cbf536f248379bdeb970442b54362873
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-02-08 20:32:57 +0530

    more basic stuff

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import re
 import functools
 from os import listdir
 import json
@@ -18,9 +19,9 @@ def setup():
     assert (file_count % NUM_FILES_PER_CHALLENGE == 0)
     total_challenges = file_count // NUM_FILES_PER_CHALLENGE
 
-    for file in range(total_challenges):
+    for challenge_idx in range(total_challenges):
         data = {}
-        PREF = f"{CHALLENGE_PATH}{file}"
+        PREF = f"{CHALLENGE_PATH}{challenge_idx}"
 
         with open(f"{PREF}.in") as f:
             data["in"] = f.read()
@@ -31,11 +32,11 @@ def setup():
             data["title"] = lines[0].strip()
             data["desc"] = "\n".join(lines[1:]).strip()
 
-        CHALLENGE_DATA[file] = data
+        CHALLENGE_DATA[challenge_idx] = data
 
 
 @app.route("/")
-def base():
+def healthcheck():
     return "Hello World"
 
 
@@ -52,7 +53,15 @@ def validate_challenge_id(func):
 @app.route("/challenges/<int:challenge_id>")
 @validate_challenge_id
 def challenge(challenge_id):
-    return render_template("challenge.html")
+    data = CHALLENGE_DATA[challenge_id]
+
+    # how to display monospaced text in jinja?
+    # keys = ["out", "desc", "in"]
+    # for key in keys:
+    #     data[key] = re.sub("\n", "<br>", data[key])
+    # data["out"] = f"<code>\n{data['out']}</code>"
+
+    return render_template("challenge.html", intxt=data["in"], out=data["out"], title=data["title"], desc=data["desc"])
 
 
 @app.route("/challenges/<int:challenge_id>.json")
