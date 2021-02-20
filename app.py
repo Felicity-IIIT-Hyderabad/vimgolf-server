cef8de8b4e4b8f11bf707587b6a526a2359585de
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-02-21 00:26:21 +0530

    Added db integration, finalized submit endpoint

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, request, render_template
-import re
+from flask import Flask, request, render_template, abort
 import functools
-from os import listdir
+from os import listdir, getenv
 import json
+from server.models.orm import db
+from server.models.models import Score
+from collections import defaultdict
+from operator import itemgetter
+from server.keys import get_keycode_repr, parse_keycodes, IGNORED_KEYSTROKES
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv(
+    "SQLALCHEMY_DATABASE_URI", "sqlite:////tmp/vimrace.db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+db.create_all(app=app)
 
 total_challenges = 0
 CHALLENGE_PATH = "challenges/"
@@ -12,7 +22,7 @@ NUM_FILES_PER_CHALLENGE = 3  # in, out, desc
 CHALLENGE_DATA = {}
 
 
-def setup():
+def init_setup():
     global total_challenges
     challenge_files = listdir(CHALLENGE_PATH)
     file_count = len(challenge_files)
@@ -40,12 +50,16 @@ def healthcheck():
     return "Hello World"
 
 
+def is_valid_challenge_id(challenge_id):
+    return challenge_id >= total_challenges or challenge_id < 0
+
+
 def validate_challenge_id(func):
     @functools.wraps(func)
     def wrapper(challenge_id):
-        if challenge_id >= total_challenges or challenge_id < 0:
-            return f"Not found challenge: {challenge_id}", 404
-        return func(challenge_id)
+        if is_valid_challenge_id(challenge_id):
+            return func(challenge_id)
+        return f"Not found challenge: {challenge_id}", 404
 
     return wrapper
 
@@ -56,12 +70,22 @@ def get_key(name, email):
     return f"key-{name}-{email}"
 
 
+def get_name_email(req):
+    name_key = "X-Fname"
+    email_key = "X-Email"
+    heads = req.headers
+    if name_key not in heads or email_key not in heads:
+        return None, None
+    return heads[name_key], heads[email_key]
+
+
 def setup_gui_route(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         filename, args = func(*args, **kwargs)
-        name = "Gaurang"  # request.headers["X-Fname"]
-        email = "gaurang.tandon@students.iiit.ac.in"  # request.headers["X-Email"]
+        name = "Gaurang"
+        email = "gaurang.tandon@students.iiit.ac.in"
+        # name, email = get_name_email(request)
         return render_template(filename, **args, name=name, apikey=get_key(name, email), logged_in=True)
 
     return wrapper
@@ -82,9 +106,54 @@ def challenge_two(challenge_id):
     return CHALLENGE_DATA[challenge_id]
 
 
+def test_keystrokes(challenge_id, keystrokestring):
+    return True
+
+
+def get_score_from_raw_keys(raw_keys):
+    # list of parsed keycode byte strings
+    keycodes = parse_keycodes(raw_keys)
+    keycodes = [keycode for keycode in keycodes if keycode not in IGNORED_KEYSTROKES]
+
+    score = len(keycodes)
+    return score
+
+
+# TODO:
+# instead of keystrokestring, upload the vim -s file as a file to the server
+# give yoogottam a temporary directory containing the submitted file
+# he will pick up in and out from challenges directory
+# yoogottam will give me a partial score (how many files gave `diff -w` exit code zero)
 @app.route("/submit", methods=["POST"])
 def submit():
-    print(request)
+    name, email = get_name_email(request)
+
+    # this shouldn't really happen
+    if name is None:
+        abort(403, "User not logged in")
+
+    raw_keys = request.args.get("entry")
+    challenge_code = request.args.get('challenge_id')
+
+    if not is_valid_challenge_id(challenge_code):
+        abort(403, "Invalid challenge id")
+
+    result = test_keystrokes(challenge_code, raw_keys)
+
+    if not result:
+        abort(403, "Invalid keystroke for given challenge id")
+
+    score_value = get_score_from_raw_keys(raw_keys)
+    exists = Score.query.filter(Score.email == email and Score.challenge_code == challenge_code).first()
+
+    if exists:
+        db.session.delete(exists)
+
+    new_score = Score(useralias=name, useremail=email, challenge_code=challenge_code, keystrokes=score_value)
+    db.session.add(new_score)
+    db.session.commit()
+
+    return 200
 
 
 @app.route("/")
@@ -97,7 +166,7 @@ def view():
         data = {"name": c_data["title"], "id": challenge_id, "best": 100}
         challenges.append(data)
 
-    return "challenge_list.html", {"title": "List of Active Challenges", "challenges": challenges, "logged_in": False}
+    return "challenge_list.html", {"title": "List of Active Challenges", "challenges": challenges}
 
 
 @app.route("/list")
@@ -108,19 +177,48 @@ def send_list():
 @app.route("/challenges_leaderboard/<int:challenge_id>.json")
 @validate_challenge_id
 def get_leaderboard(challenge_id):
-    return json.dumps([])
+    return json.dumps(get_challenge_leaderboard_data(challenge_id))
+
+
+def get_challenge_leaderboard_data(challenge_code):
+    scores = Score.query.filter(Score.challenge_code == challenge_code).all()
+    scores = sorted(scores, key=lambda score: score.keystrokes)
+    return [{"alias": score.useralias, "score": score.keystrokes} for score in scores]
+
+
+def get_global_leaderboard_data():
+    scores = Score.query.all()
+    countsolved = defaultdict(int)
+    totalkeys = defaultdict(int)
+    usernames = set([])
+
+    for score in scores:
+        countsolved[score.useralias] += 1
+        totalkeys[score.useralias] += score.keystrokes
+        usernames.add(score.useralias)
+
+    usernames = list(usernames)
+    score_list = [((-countsolved[alias], totalkeys[alias]), alias) for alias in usernames]
+
+    score_list = sorted(score_list, key=itemgetter(0))
+
+    leaders = []
+    for rank, sorted_item in enumerate(score_list):
+        alias = sorted_item[1]
+        leaders.append({"rank": rank, "username": alias, "score": totalkeys[alias], "solved": countsolved[alias]})
+
+    return leaders
 
 
 @app.route("/leaderboard")
 @setup_gui_route
 def leaderboard():
-    leaders = [{"rank": 1, "username": "sigma_g", "score": 100, 'scores': [-100]},
-               {"rank": 2, "username": "yoogottamk", "score": -100, "scores": [-100]}]
     cha_ids = list(range(total_challenges))
+    leaders = get_global_leaderboard_data()
     return render_template("leaderboard.html", leaders=leaders, title="Global leaderboard", cha_ids=cha_ids)
 
 
-setup()
+init_setup()
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ keys.py
@@ -0,0 +1,219 @@
+"""Maps keys recorded by vim to a printable representation"""
+
+
+def to_bytes(x):
+    """Convert an integer to bytes."""
+    return x.to_bytes(2, 'big')
+
+
+def to_int(x):
+    """Convert bytes to an integer."""
+    return int(x.hex(), 16)
+
+
+# Vim records key presses using 1) a single byte or 2) a 0x80 byte
+# followed by two bytes. Parse the single-bytes and double-bytes.
+# For the returned list, all values are represented with two bytes
+# (single bytes are padded).
+def parse_keycodes(raw_keys):
+    """
+    Parse list of keypress bytes from raw keypress representation saved
+    by vim's -w.
+    """
+    keycodes = []
+    tmp = list(reversed(raw_keys))
+    while tmp:
+        b0 = tmp.pop()
+        if b0 == 0x80:
+            b1 = tmp.pop()
+            b2 = tmp.pop()
+            keycode = bytes((b1, b2))
+        else:
+            keycode = to_bytes(b0)
+        keycodes.append(keycode)
+    return keycodes
+
+
+# keystrokes that should not impact score (e.g., window focus)
+IGNORED_KEYSTROKES = {
+    b'\xfd\x35',  # (35) KE_IGNORE
+    # Incidentally, CursorHold is triggered on GVIM move window
+    b'\xfd\x60',  # (96) KE_CURSORHOLD Move Window (GVIM)
+    b'\xfd\x61',  # (97) KE_NOP
+    b'\xfd\x62',  # (98) KE_FOCUSGAINED Focus Gained (GVIM)
+    b'\xfd\x63',  # (99) KE_FOCUSLOST Focus Lost (GVIM)
+}
+
+_KEYCODE_REPR_LOOKUP = {}
+# Control characters
+for x in range(32):
+    _KEYCODE_REPR_LOOKUP[to_bytes(x)] = '<C-{}>'.format(chr(x + 64))
+# Printable characters
+for x in range(32, 127):
+    _KEYCODE_REPR_LOOKUP[to_bytes(x)] = chr(x)
+# Single byte fallback
+for x in range(127, 256):
+    _KEYCODE_REPR_LOOKUP[to_bytes(x)] = '<0x{:02x}>'.format(x)
+# Specially named control characters and others
+_KEYCODE_REPR_LOOKUP[to_bytes(9)] = '<Tab>'
+_KEYCODE_REPR_LOOKUP[to_bytes(10)] = '<NL>'
+_KEYCODE_REPR_LOOKUP[to_bytes(13)] = '<CR>'
+_KEYCODE_REPR_LOOKUP[to_bytes(27)] = '<Esc>'
+_KEYCODE_REPR_LOOKUP[to_bytes(127)] = '<Del>'
+
+_KEYCODE_REPR_LOOKUP.update({
+    b'k1': '<F1>',
+    b'k2': '<F2>',
+    b'k3': '<F3>',
+    b'k4': '<F4>',
+    b'k5': '<F5>',
+    b'k6': '<F6>',
+    b'k7': '<F7>',
+    b'k8': '<F8>',
+    b'k9': '<F9>',
+    b'k;': '<F10>',
+    b'F1': '<F11>',
+    b'F2': '<F12>',
+    b'F3': '<F13>',
+    b'F4': '<F14>',
+    b'F5': '<F15>',
+    b'F6': '<F16>',
+    b'F7': '<F17>',
+    b'F8': '<F18>',
+    b'F9': '<F19>',
+
+    b'%1': '<Help>',
+    b'&8': '<Undo>',
+    b'#2': '<S-Home>',
+    b'*7': '<S-End>',
+    b'K1': '<kHome>',
+    b'K4': '<kEnd>',
+    b'K3': '<kPageUp>',
+    b'K5': '<kPageDown>',
+    b'K6': '<kPlus>',
+    b'K7': '<kMinus>',
+    b'K8': '<kDivide>',
+    b'K9': '<kMultiply>',
+    b'KA': '<kEnter>',
+    b'KB': '<kPoint>',
+    b'KC': '<k0>',
+    b'KD': '<k1>',
+    b'KE': '<k2>',
+    b'KF': '<k3>',
+    b'KG': '<k4>',
+    b'KH': '<k5>',
+    b'KI': '<k6>',
+    b'KJ': '<k7>',
+    b'KK': '<k8>',
+    b'KL': '<k9>',
+
+    b'kP': '<PageUp>',
+    b'kN': '<PageDown>',
+    b'kh': '<Home>',
+    b'@7': '<End>',
+    b'kI': '<Insert>',
+    b'kD': '<Del>',
+    b'kb': '<BS>',
+
+    b'ku': '<Up>',
+    b'kd': '<Down>',
+    b'kl': '<Left>',
+    b'kr': '<Right>',
+    b'#4': '<S-Left>',
+    b'%i': '<S-Right>',
+
+    b'kB': '<S-Tab>',
+    b'\xffX': '<C-@>',
+
+    # Escaped literal 0x80
+    b'\xfeX': '<0x80>',
+
+    # "These rarely-used modifiers should be combined with the next
+    #  stroke (like <S-Space>), but let's put them here for now"
+    b'\xfc\x02': '<S->',
+    b'\xfc\x04': '<C->',
+    b'\xfc\x06': '<C-S->',
+    b'\xfc\x08': '<A->',
+    b'\xfc\x0a': '<A-S->',
+    b'\xfc\x0c': '<C-A>',
+    b'\xfc\x0e': '<C-A-S->',
+    b'\xfc\x10': '<M->',
+    b'\xfc\x12': '<M-S->',
+    b'\xfc\x14': '<M-C->',
+    b'\xfc\x16': '<M-C-S->',
+    b'\xfc\x18': '<M-A->',
+    b'\xfc\x1a': '<M-A-S->',
+    b'\xfc\x1c': '<M-C-A>',
+    b'\xfc\x1e': '<M-C-A-S->',
+
+    b'\xfd\x04': '<S-Up>',
+    b'\xfd\x05': '<S-Down>',
+    b'\xfd\x06': '<S-F1>',
+    b'\xfd\x07': '<S-F2>',
+    b'\xfd\x08': '<S-F3>',
+    b'\xfd\x09': '<S-F4>',
+    b'\xfd\x0a': '<S-F5>',
+    b'\xfd\x0b': '<S-F6>',
+    b'\xfd\x0c': '<S-F7>',
+    b'\xfd\x0d': '<S-F9>',
+    b'\xfd\x0e': '<S-F10>',
+    b'\xfd\x0f': '<S-F10>',
+    b'\xfd\x10': '<S-F11>',
+    b'\xfd\x11': '<S-F12>',
+    b'\xfd\x12': '<S-F13>',
+    b'\xfd\x13': '<S-F14>',
+    b'\xfd\x14': '<S-F15>',
+    b'\xfd\x15': '<S-F16>',
+    b'\xfd\x16': '<S-F17>',
+    b'\xfd\x17': '<S-F18>',
+    b'\xfd\x18': '<S-F19>',
+    b'\xfd\x19': '<S-F20>',
+    b'\xfd\x1a': '<S-F21>',
+    b'\xfd\x1b': '<S-F22>',
+    b'\xfd\x1c': '<S-F23>',
+    b'\xfd\x1d': '<S-F24>',
+    b'\xfd\x1e': '<S-F25>',
+    b'\xfd\x1f': '<S-F26>',
+    b'\xfd\x20': '<S-F27>',
+    b'\xfd\x21': '<S-F28>',
+    b'\xfd\x22': '<S-F29>',
+    b'\xfd\x23': '<S-F30>',
+    b'\xfd\x24': '<S-F31>',
+    b'\xfd\x25': '<S-F32>',
+    b'\xfd\x26': '<S-F33>',
+    b'\xfd\x27': '<S-F34>',
+    b'\xfd\x28': '<S-F35>',
+    b'\xfd\x29': '<S-F36>',
+    b'\xfd\x2a': '<S-F37>',
+    b'\xfd\x2b': '<Mouse>',
+    b'\xfd\x2c': '<LeftMouse>',
+    b'\xfd\x2d': '<LeftDrag>',
+    b'\xfd\x2e': '<LeftRelease>',
+    b'\xfd\x2f': '<MiddleMouse>',
+    b'\xfd\x30': '<MiddleDrag>',
+    b'\xfd\x31': '<MiddleRelease>',
+    b'\xfd\x32': '<RightMouse>',
+    b'\xfd\x33': '<RightDrag>',
+    b'\xfd\x34': '<RightRelease>',
+
+    b'\xfd\x4b': '<ScrollWheelUp>',
+    b'\xfd\x4c': '<ScrollWheelDown>',
+    b'\xfd\x4d': '<ScrollWheelRight>',
+    b'\xfd\x4e': '<ScrollWheelLeft>',
+    b'\xfd\x4f': '<kInsert>',
+    b'\xfd\x50': '<kDel>',
+    b'\xfd\x55': '<C-Left>',
+    b'\xfd\x56': '<C-Right>',
+    b'\xfd\x57': '<C-Home>',
+    b'\xfd\x58': '<C-End>',
+})
+
+
+def get_keycode_repr(keycode):
+    if keycode in _KEYCODE_REPR_LOOKUP:
+        key = _KEYCODE_REPR_LOOKUP[keycode]
+    else:
+        # Show unknown keycodes as hex codes surrounded by brackets.
+        key = ''.join('\\x{:02x}'.format(x) for x in keycode)
+        key = '[' + key + ']'
+    return key
